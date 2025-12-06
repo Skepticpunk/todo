cb4f9f08f120431553cb779d1a61ea3e8024ce00
skepticpunk <14169054+Skepticpunk@users.noreply.github.com>
2025-12-05 19:14:45 -0500

    expanded new list type, added styling

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import "./page.css";
 
 class toDoEntry {
-  constructor( title, desc, date, priority ) {
+  constructor( priority, title, description, dateAdded, dueDate) {
+    this.#priority = priority;
     this.#title = title;
-    this.#description = desc;
-    this.#dueDate = date,
-    this.#priority = priority; };
+    this.#description = description;
+    this.#dateAdded = dateAdded;
+    this.#dueDate = dueDate; };
+  #priority = 0;
   #title = "Title";
   #description = "Description";
-  #dueDate = "1/1/1900";
-  #priority = 0;
+  #dateAdded = "1/1/1900";
+  #dueDate = "12/31/2099";
   #status = 0;
   getTitle() { return this.#title };
   setTitle( newTitle ) { this.#title = newTitle };
   getDesc() { return this.#description };
   setDesc( newDesc ) { this.#description = newDesc };
-  getDate() { return this.#dueDate };
-  setDate( newDate ) { this.#dueDate = newDate };
+  getAdded() { return this.#dateAdded; }
+  setAdded( newAdded ) { this.#dateAdded = newAdded }
+  getDue() { return this.#dueDate };
+  setDue( newDue ) { this.#dueDate = newDue };
   getPriority() { return this.#priority };
   setPriority( newPriority ) { this.#priority = newPriority };
   getStatus() { return this.#status };
-  setStatus( newStatus ) { this.#status = newStatus }; };
+  setStatus( newStatus ) { this.#status = newStatus };
+};
 class toDoList {
   constructor( title ) { this.#title = title };
   #list = [];
@@ -34,7 +39,8 @@ class toDoList {
     this.#list.splice( entry, 1 );
     this.#list.splice( position - 1, 0, targetEntry ); }; 
   getTitle() { return this.#title }
-  setTitle( newTitle ) { this.#title = newTitle; } };
+  setTitle( newTitle ) { this.#title = newTitle; }
+};
 class listDisplay {
   constructor( parentNode, headerNode, list ) {
     this.#parent = parentNode;
@@ -55,27 +61,45 @@ class listDisplay {
     this.#parent.textContent = "";
     this.#header.textContent = this.#list.getTitle();
     this.#list.getList().forEach( ( entry ) => {
-      if ( !type ) {
-        this.#parent.append( entry.getTitle() ); }
-      else if ( listType == entry ) { 
-        entryCell = document.createElement("div");
-        entryCell.id = "#firstCell";
-        this.#parent.append( entryCell ); }; } ); }; };
+     if ( entry instanceof toDoEntry ) { 
+        const entryCell = document.createElement("div");
+        const cellPriority = document.createElement("div");
+        const cellTitle = document.createElement("div");
+        const cellDesc = document.createElement("div");
+        const cellAdded = document.createElement("div");
+        const cellDue = document.createElement("div");
+        const cellStatus = document.createElement("div");
+        entryCell.id = "entry"; 
+        cellTitle.id = "cellTitle";
+        cellTitle.textContent = entry.getTitle();
+        cellPriority.textContent = entry.getPriority();
+        cellDesc.textContent = entry.getDesc();
+        cellAdded.textContent = entry.getAdded();
+        cellDue.textContent = entry.getDue();
+        cellStatus.textContent = entry.getStatus();
+        entryCell.append( cellPriority );
+        entryCell.append( cellTitle );
+        entryCell.append( cellDesc );
+        entryCell.append( cellAdded );
+        entryCell.append( cellDue );
+        entryCell.append( cellStatus );
+        this.#parent.append( entryCell ); }
+      else {
+        this.#parent.append( entry.getTitle() ); };
+    } );
+  };
+};
 
 const toDoLists = new toDoList("Lists");
 const toDoList1 = new toDoList("List 1");
 const listsDisplay = new listDisplay( document.querySelector( "#toDoLists" ), document.querySelector( "#listsHeader > h1" ), toDoLists );
 const toDoListDisplay = new listDisplay( document.querySelector( "#toDoEntries" ), document.querySelector( "#toDoTitle" ), toDoList1 );
-const entry1 = new toDoEntry("asdf", "ARE WE WORKING YET????", "4/20/69", 0);
+const entry1 = new toDoEntry(0, "asdf", "ARE WE WORKING YET????", "4/20/69", "3/14/15", 0);
 
 function addList( list ) { toDoLists.push( list ) };
 function delList( list ) { toDoLists.splice( list, 1 ) };
 
 toDoLists.addEntry( toDoList1 );
 toDoLists.getEntry(0).addEntry( entry1 );
-console.log(toDoLists.getEntry(0).getEntry(0).getTitle());
-console.log(toDoLists.getEntry(0).getEntry(0).getDesc());
-console.log(toDoLists.getEntry(0).getList());
-console.log(toDoLists.getList())
 listsDisplay.renderList();
 toDoListDisplay.renderList();
