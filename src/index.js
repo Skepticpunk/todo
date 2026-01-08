8d37eec8d7256301362f093c3505374c1c5be725
Skepticpunk <14169054+Skepticpunk@users.noreply.github.com>
2026-01-08 18:28:48 -0500

    properly started entryDisplay

--- src/index.js
+++ src/index.js
@@ -4,36 +4,54 @@ class toDoEntry {
   #priority = 0;
   #title = "Title";
   #description = "Description";
-  #dateAdded = "1/1/1900";
-  #dueDate = "12/31/2099";
+  #added = "1/1/1900";
+  #due = "12/31/2099";
   #status = 0;
+  #attributes = [
+    this.priority,
+    this.#title, 
+    this.#description,
+    this.#added,
+    this.#due,
+    this.#status
+  ]
 
   get title() { return this.#title };
   set title(newTitle) { this.#title = newTitle };
   get desc() { return this.#description };
   set desc(newDesc) { this.#description = newDesc };
-  get added() { return this.#dateAdded; }
-  set added(newAdded) { this.#dateAdded = newAdded }
-  get due() { return this.#dueDate };
-  set due(newDue) { this.#dueDate = newDue };
+  get added() { return this.#added; }
+  set added(newAdded) { this.#added = newAdded }
+  get due() { return this.#due };
+  set due(newDue) { this.#due = newDue };
   get priority() { return this.#priority };
   set priority(newPriority) { this.#priority = newPriority };
   get status() { return this.#status };
   set status(newStatus) { this.#status = newStatus };
+  get attributes() { return this.#attributes };
   
-  constructor(priority, title, description, dateAdded, dueDate) {
+  constructor(priority, title, description, added, due) {
     this.#priority = priority;
     this.#title = title;
     this.#description = description;
-    this.#dateAdded = dateAdded;
-    this.#dueDate = dueDate;
+    this.#added = added;
+    this.#due = due;
   };
 };
 class entryDisplay {
-  constructor(elementType, entryContainer, entryElements) {
-    elements = []
-    entryElements.forEach(elements.append(elements[i]));
-    eval("this." + arguments[i] + " = elements[i]");
+  #entryCell = document.createElement("div");
+  #cellRemoveButton = document.createElement("button");
+  #expDesc = document.querySelector("toDoDesc");
+  constructor(entry) {
+    // group and assign common elements
+    elements = ["cellPriority", "cellTitle", "cellDesc", "cellAdded", "cellDue", "cellStatus"]
+    elements.map((element, i) => {
+      const elemName = element[i]
+      const newElement = document.createElement("div")
+      newElement.textContent = entry.attributes[i]
+      entryCell.append(newElement)
+      eval()
+    })
   };
 };
 class toDoList {
@@ -70,21 +88,33 @@ class listDisplay {
   set header(newHeader) { this.#header = newHeader };
   
   addEntry() {
-    entry = new entryDisplay("input", "entryCell", "cellPriority", "cellTitle", "cellDesc", "cellAdded", "cellDue");
+    entry = new entryDisplay(
+      "input",
+      "entryCell",
+      "cellPriority",
+      "cellTitle",
+      "cellDesc",
+      "cellAdded",
+      "cellDue");
     entry.entryCell.id = "entry";
     entry.cellTitle.id = "entryTitle";
     this.#header.textContent = "";
     this.#header.append(entryCell)
     this.#addButton.removeEventListener("click")
     this.#addButton.addEventListener("click", () => {
-      newEntry = new toDoEntry(entry.cellPriority.textContent, entry.cellTitle.textContent, entry.cellDesc.textContent, entry.cellAdded.textContent, entry.cellDue.textContent);
+      newEntry = new toDoEntry(
+        entry.cellPriority.textContent,
+        entry.cellTitle.textContent,
+        entry.cellDesc.textContent,
+        entry.cellAdded.textContent,
+        entry.cellDue.textContent);
       this.#list.push(newEntry)
     });
   };
   renderList() {
     this.#listDisplay.textContent = "";
-    this.#header.textContent = this.#list.getTitle();
-    this.#list.getList().forEach((entry) => {
+    this.#header.textContent = this.#list.Title;
+    this.#list.list.forEach((entry) => {
       if (entry instanceof toDoEntry) {
         const entryCell = document.createElement("div");
         const cellPriority = document.createElement("div");
@@ -111,13 +141,13 @@ class listDisplay {
         entryCell.append(cellStatus);
         entryCell.append(cellRemoveButton);
         cellRemoveButton.textContent = "-";
-        entryCell.addEventListener("mouseover", () => { expDesc.textContent = entry.getDesc() })
+        entryCell.addEventListener("mouseover", () => { expDesc.textContent = entry.desc })
         entryCell.addEventListener("mouseout", () => { expDesc.textContent = "" })
         cellRemoveButton.addEventListener("click", () => { expDesc.textContent = ""; entryCell.remove() })
         this.#listDisplay.append(entryCell);
       }
       else {
-        this.#listDisplay.append(entry.getTitle());
+        this.#listDisplay.append(entry.title);
       };
     });
   };
@@ -136,9 +166,23 @@ class listDisplay {
 
 const projectLists = new toDoList("Lists");
 const toDoList1 = new toDoList("List 1");
-const projectsDisplay = new listDisplay(document.querySelector("#projectList"), document.querySelector("#projectHeader > h1"), document.querySelector("#projects"), projectLists);
-const toDoListDisplay = new listDisplay(document.querySelector("#toDo"), document.querySelector("#toDoTitle"), document.querySelector("#toDoEntries"), toDoList1);
-const entry1 = new toDoEntry(0, "Debug Entry", "Lorem Ipsum Dolor Sit Amet blah blah blah here's more words I LOVE BEIJING TIANANMEN", "4/20/69", "3/14/15", 0);
+const projectsDisplay = new listDisplay(
+  document.querySelector("#projectList"), 
+  document.querySelector("#projectHeader > h1"), 
+  document.querySelector("#projects"), 
+  projectLists);
+const toDoListDisplay = new listDisplay(
+  document.querySelector("#toDo"), 
+  document.querySelector("#toDoTitle"), 
+  document.querySelector("#toDoEntries"), 
+  toDoList1);
+const entry1 = new toDoEntry(
+  0,
+  "Debug Entry",
+  "Lorem Ipsum Dolor Sit Amet blah blah blah here's more words I LOVE BEIJING TIANANMEN",
+  "4/20/69",
+  "3/14/15",
+  0);
 
 function addList(list) { projectLists.push(list) };
 function delList(list) { projectLists.splice(list, 1) };
