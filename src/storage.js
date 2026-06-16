b0f0630c29cc28d6f9975b603149062b80d5c93a
Skepticpunk <14169054+Skepticpunk@users.noreply.github.com>
2026-06-16 17:20:13 -0400

    new comments

--- src/storage.js
+++ src/storage.js
@@ -1,13 +1,19 @@
 class listStorageHandler {
   constructor(storedLists) {
+
+    // We want to try loading a main list when the page is loaded
+    // and then load every sublist in it
+
     // if the list exists, load it
-    if(storedLists) {
-       this.#storedLists = storedLists;
+    if (storedLists) {
+      this.#storedLists = storedLists;
     }
   }
 
   #storedLists = [];
-  
+
+  // the main list needs to have CRUD functions, as do the sublists
+
   set storedLists(storedLists) { this.#storedLists = storedLists };
   get storedLists() { return this.#storedLists };
 
